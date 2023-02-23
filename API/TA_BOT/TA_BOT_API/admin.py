badf458d048e2048b57d7f16a4049d6dc2923d04
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2023-02-23 22:34:43 +0330

    Add active sessions to django admin panel

--- API/TA_BOT/TA_BOT_API/admin.py
+++ API/TA_BOT/TA_BOT_API/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 # Register your models here.
 from import_export import resources
-from TA_BOT_API.models import AuthData, Category
+from TA_BOT_API.models import AuthData, Category, TelegramActiveSessions
 from import_export.admin import ImportExportModelAdmin
 from django.dispatch import receiver
 from import_export.signals import post_import
@@ -25,3 +25,4 @@ class AuthDataAdmin(ImportExportModelAdmin):
 
 admin.site.register(AuthData, AuthDataAdmin)
 admin.site.register(Category)
+admin.site.register(TelegramActiveSessions)
